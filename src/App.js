14df0f3c40307dec36432804b65aa850b9ba2d8e
gajendra <gajendra@forktechnologies.com>
2019-12-08 10:40:32 +0530

    display shop preview

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-
-const Hats = () => (
-  <div>
-    <h1>Hats PAGE</h1>
-  </div>
-)
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={Hats} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
