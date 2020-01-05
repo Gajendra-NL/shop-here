86f10f635873aa7fd9cfafb496e0850f0b229820
gajendra <gajendra@forktechnologies.com>
2020-01-05 23:45:47 +0530

    use createStructuredSelector helper from reselect

--- src/redux/cart/cart.selector.js
+++ src/redux/cart/cart.selector.js
@@ -11,3 +11,8 @@ export const selectCartItemsCount = createSelector(
     [selectCartItems],
     (cartItems) => cartItems.reduce((a, e) => a + e.quantity, 0)
 )
+
+export const selectCartHidden = createSelector(
+    [selectCart],
+    cart => cart.hidden
+)
