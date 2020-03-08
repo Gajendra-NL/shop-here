07d4c7ce20e82b59836f8d486fdbd5146d1fd515
gajendra <gajendra@forktechnologies.com>
2020-03-08 17:15:17 +0530

    switch from observable pattern to promises pattern

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -25,11 +25,22 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    collectionRef.onSnapshot(async snapshot => {
+    collectionRef.get().then(snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
       this.setState({ loading: false });
     })
+
+    // fetch('https://firestore.googleapis.com/v1/projects/shop-here-db/databases/(default)/documents/collections')
+    //   .then(res => res.json())
+    //   .then(coll => console.log('check res', coll))
+
+
+    // collectionRef.onSnapshot(async snapshot => {
+    //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //   updateCollections(collectionsMap);
+    //   this.setState({ loading: false });
+    // })
   }
 
   render() {
