810624323cedc28dcc298a2ac1e57afd585fffd2
Gajendra <gajendra@DVs-MacBook-Pro.local>
2020-04-04 22:48:39 +0530

    persist login using sagas

--- src/App.js
+++ src/App.js
@@ -13,12 +13,14 @@ import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
 
 import { selectCurrentUser } from './redux/user/user.selectors';
+import { checkUserSession } from './redux/user/user.actions';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-
+    const { checkUserSession } = this.props;
+    checkUserSession();
     // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
     //   if (userAuth) {
     //     const userRef = await createUserProfileDocument(userAuth);
@@ -67,4 +69,6 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
 });
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, {
+  checkUserSession,
+})(App);
